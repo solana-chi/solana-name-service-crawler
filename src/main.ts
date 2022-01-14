576c1252a738fe13f2cc003f6aa294ec17441a46
Willake <willakelin@gmail.com>
2022-01-14 20:57:07 +0800

    add lines for decoding account data

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
-import { NAME_PROGRAM_ID, performReverseLookup, getFilteredProgramAccounts } from '@bonfida/spl-name-service';
+import { NAME_PROGRAM_ID, performReverseLookup, getFilteredProgramAccounts, NameRegistryState } from '@bonfida/spl-name-service';
 import { Connection, PublicKey, AccountInfo, clusterApiUrl } from '@solana/web3.js';
 import { Buffer } from 'buffer';
+import * as borsh from 'borsh';
 
 const SPL_CLASS_ID = new PublicKey('33m47vH6Eav6jr5Ry86XjhRft2jRBLDnDgPSHoquXi2Z');
 
 export async function findAllNameAccounts(
     connection: Connection
-  ): Promise<PublicKey[]> {
+  ): Promise<string[]> {
 
     const filters = [
       {
+        // memcmp: {
+        //   offset: 64,
+        //   bytes: SPL_CLASS_ID.toBase58(),
+        // },
         memcmp: {
-          offset: 64,
-          bytes: SPL_CLASS_ID.toBase58(),
-        },
+          offset: 32,
+          bytes: new PublicKey('Fe4yQVFiJTYRCqxuGKVsx9cRnf8sxttKbsmZtwWKo91r').toBase58(),
+        }
       },
     ];
 
@@ -23,8 +28,13 @@ export async function findAllNameAccounts(
       }> = await connection.getProgramAccounts(NAME_PROGRAM_ID, {
       filters,
     });
+
+    const addresses = accounts.map(info => 
+      borsh.deserializeUnchecked(NameRegistryState.schema, NameRegistryState, info.account.data).owner.toString());
+
+    const uniqAddresses = [...new Set(addresses)];
     
-    return accounts.map((a) => a.pubkey);
+    return uniqAddresses;
 }
 
 async function main() {
